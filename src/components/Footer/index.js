be43333653682ef8c107ba7f1c9946555861c5b6
Boris Madonov <maboig@yandex.ru>
2021-01-15 22:43:17 +0300

    feat: разработать компонент footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FbIcon, GhIcon } from '../../images/icon';
+import './index.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">&copy; 2020 Supersite, Powered by News API</p>
+      <nav className="footer__nav">
+        <ul className="footer__nav-list footer__nav_list_text">
+          <li className="footer__nav-element-text">
+            <Link
+              className="footer__nav-link"
+              to="/"
+            >
+              <p className="footer__nav-link-text">
+                Главная
+              </p>
+            </Link>
+          </li>
+          <li className="footer__nav-element-text">
+            <a
+              className="footer__nav-link"
+              href="https://praktikum.yandex.ru/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="footer__nav-link-text">
+                Яндекс.Практикум
+              </p>
+            </a>
+          </li>
+        </ul>
+        <ul className="footer__nav-list footer__nav_list_icon">
+          <li className="footer__nav-element-icon">
+            <a
+              className="footer__nav-link"
+              href="https://github.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GhIcon
+                className="footer__nav-link-icon"
+              />
+            </a>
+          </li>
+          <li className="footer__nav-element-icon">
+            <a
+              className="footer__nav-link"
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FbIcon
+                className="footer__nav-link-icon"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/images/icon/index.js
@@ -0,0 +1,2 @@
+export { ReactComponent as FbIcon } from './fbicon.svg';
+export { ReactComponent as GhIcon } from './ghicon.svg';
